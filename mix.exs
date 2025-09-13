6fb680ebc0b54ed36b743c9bd668610ba0e576af
forumevi <35641620+forumevi@users.noreply.github.com>
2025-09-14 00:55:18 +0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -6,14 +6,11 @@ defmodule Upward.MixProject do
       app: :upward,
       version: "0.1.0",
       elixir: "~> 1.15",
-      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      aliases: aliases(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       mod: {Upward.Application, []},
@@ -21,11 +18,6 @@ defmodule Upward.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
-
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:phoenix, "~> 1.7.21"},
@@ -35,14 +27,6 @@ defmodule Upward.MixProject do
       {:plug_cowboy, "~> 2.7"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
       {:gettext, "~> 0.26"}
-    ]
-  end
-
-  # Aliases are shortcuts or tasks specific to the project.
-  defp aliases do
-    [
-      setup: ["deps.get", "cmd npm --prefix assets install"],
-      "assets.deploy": ["cmd npm --prefix assets run deploy", "esbuild default --minify", "phx.digest"]
     ]
   end
 end
