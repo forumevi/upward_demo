7d295a471ee1cf48b44288090970315eda1fecec
forumevi <35641620+forumevi@users.noreply.github.com>
2025-09-14 00:56:16 +0300

    Create dev.exs

---
+++ config/dev.exs
@@ -0,0 +1,10 @@
+import Config
+
+config :upward, UpwardWeb.Endpoint,
+  http: [port: 4000],
+  debug_errors: true,
+  code_reloader: true,
+  check_origin: false,
+  watchers: [
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+  ]
