6c5439e49d4a4b0361ac3dbd63fac39e624c2b8a
forumevi <35641620+forumevi@users.noreply.github.com>
2025-09-14 00:25:58 +0300

    Update intent_live.ex

--- lib/upward_web/live/intent_live.ex
+++ lib/upward_web/live/intent_live.ex
@@ -1,23 +1,18 @@
 defmodule UpwardWeb.IntentLive do
-  use UpwardWeb, :live_view
-  alias Upward.Intents
-  alias Phoenix.PubSub
+  use Phoenix.LiveView
 
-  @topic "intents"
-
+  @impl true
   def mount(_params, _session, socket) do
-    if connected?(socket), do: PubSub.subscribe(Upward.PubSub, @topic)
-    intents = Intents.list()
-    {:ok, assign(socket, intents: intents, composer: "")}
+    {:ok, assign(socket, :intent, "Type your intent...")}
   end
 
-  def handle_event("compose", %{"intent" => %{"text" => text}}, socket) do
-    intent = Intents.create(%{text: text})
-    PubSub.broadcast(Upward.PubSub, @topic, {:new_intent, intent})
-    {:noreply, assign(socket, composer: "", intents: [intent | socket.assigns.intents])}
-  end
-
-  def handle_info({:new_intent, intent}, socket) do
-    {:noreply, update(socket, :intents, fn list -> [intent | list] end)}
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div>
+      <h1>Upward Intent Demo</h1>
+      <p><%= @intent %></p>
+    </div>
+    """
   end
 end
