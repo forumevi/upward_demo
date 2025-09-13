bb7b0c97bfdb46bcf8aaf62b378b28fc65fc2353
forumevi <35641620+forumevi@users.noreply.github.com>
2025-09-14 01:06:14 +0300

    Update endpoint.ex

--- lib/upward_web/endpoint.ex
+++ lib/upward_web/endpoint.ex
@@ -1,22 +1,17 @@
 defmodule UpwardWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :upward
 
-  @session_options [
-    store: :cookie,
-    key: "_upward_key",
-    signing_salt: "SECRET_SALT"
-  ]
-
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket
 
   plug Plug.Static,
     at: "/",
     from: :upward,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt)
 
   plug Plug.RequestId
-  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+  plug Plug.Logger
+
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
@@ -24,6 +19,7 @@ defmodule UpwardWeb.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
-  plug Plug.Session, @session_options
+  plug Plug.Session, store: :cookie, key: "_upward_key", signing_salt: "SECRET_SALT"
+
   plug UpwardWeb.Router
 end
