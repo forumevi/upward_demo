700bcd95e1db6def83ac6711a123975cdba6d32d
forumevi <35641620+forumevi@users.noreply.github.com>
2025-09-14 01:06:30 +0300

    Update router.ex

--- lib/upward_web/router.ex
+++ lib/upward_web/router.ex
@@ -1,12 +1,12 @@
 defmodule UpwardWeb.Router do
   use UpwardWeb, :router
-
   import Phoenix.LiveView.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
+    plug :put_root_layout, {UpwardWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
