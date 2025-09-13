553fe2a9392461c2eae81f8c6e04b6b2166983af
forumevi <35641620+forumevi@users.noreply.github.com>
2025-09-13 23:39:50 +0300

    Create application.ex

---
+++ lib/upward/application.ex
@@ -0,0 +1,18 @@
+defmodule Upward.Application do
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      UpwardWeb.Endpoint,
+      Upward.Intents
+    ]
+
+    opts = [strategy: :one_for_one, name: Upward.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  def config_change(changed, _new, removed) do
+    UpwardWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
