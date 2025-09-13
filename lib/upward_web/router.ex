8ce87ba07402038a635fa54095d66b103838ae6f
forumevi <35641620+forumevi@users.noreply.github.com>
2025-09-14 00:44:21 +0300

    Update router.ex

--- lib/upward_web/router.ex
+++ lib/upward_web/router.ex
@@ -1,12 +1,13 @@
 defmodule UpwardWeb.Router do
   use UpwardWeb, :router
+
   import Phoenix.LiveView.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
-    plug :put_root_layout, {UpwardWeb.LayoutView, :root}
+    plug :put_root_layout, {UpwardWeb.LayoutView, :live}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
@@ -14,6 +15,7 @@ defmodule UpwardWeb.Router do
   scope "/", UpwardWeb do
     pipe_through :browser
 
-    live "/", IntentLive
+    live "/intent", IntentLive, layout: {UpwardWeb.LayoutView, :live}
+    # Diğer LiveView route’lar burada
   end
 end
